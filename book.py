0309d3e60948a6b81f3e98a45835576f2598b75e
Noah Holt <tidalturtles@gmail.com>
2020-09-10 17:05:36 -0600

    Update book.py

--- book.py
+++ book.py
@@ -2,8 +2,25 @@
 
 ## The Book of choice is Python Crash Coarse from No Starch Press  ##
 
-print('Hello World')
+# print('Hello World')
+
+## To run type python ____.py in terminal ##
 
 ##  The line above is simple print() does what it says and  ##
 ## the '' make Hello World a string to be printed  ##
+
+## Today 10 Sept 2020 ##
+
+##  Chapter 2: Variables and simple data types  ##
+
+message = "Hello World Again"
+print(message)
+
+message = 'Sike? how does one spell this word?'
+print(message)
+
+
+## We made the word message a variable with the value of the string (str) ##
+## now we can print the variable message and not need to type the phrase every time  ##
+
 
