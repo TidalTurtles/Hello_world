f55a8848fc7759ebce0980452f7a7caef272c3af
Noah Holt <tidalturtles@gmail.com>
2020-09-10 17:09:50 -0600

    Update book.py

--- book.py
+++ book.py
@@ -1,6 +1,7 @@
 ##  This is were I will do my bookwork for python  ##
 
 ## The Book of choice is Python Crash Coarse from No Starch Press  ##
+## KEY: double # is for notes and comments, single are old code ##
 
 # print('Hello World')
 
@@ -20,7 +21,9 @@ message = 'Sike? how does one spell this word?'
 print(message)
 
 
+
 ## We made the word message a variable with the value of the string (str) ##
 ## now we can print the variable message and not need to type the phrase every time  ##
-
+## change variable value and anything using it after the change ##
+## uses the new value.
 
