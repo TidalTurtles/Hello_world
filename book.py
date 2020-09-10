d07eb1a9baa0bd4048814e3da9aa896dae71f3e2
Noah Holt <tidalturtles@gmail.com>
2020-09-10 16:46:46 -0600

    Create book.py

---
+++ book.py
@@ -0,0 +1,9 @@
+##  This is were I will do my bookwork for python  ##
+
+## The Book of choice is Python Crash Coarse from No Starch Press  ##
+
+print('Hello World')
+
+##  The line above is simple print() does what it says and  ##
+## the '' make Hello World a string to be printed  ##
+
